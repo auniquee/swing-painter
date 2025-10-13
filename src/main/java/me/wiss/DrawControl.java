a2f68d08d23e89a31776c1e59190dffe466e9371
aunique <coolatanwiss@gmail.com>
2025-10-13 10:39:59 +0200

    Rename Main -> DrawControl

---
+++ src/main/java/me/wiss/DrawControl.java
@@ -0,0 +1,9 @@
+package me.wiss;
+
+import me.wiss.control.Controller;
+
+public class DrawControl {
+    public static void main(String[] args) {
+        Controller controller = new Controller();
+    }
+}
